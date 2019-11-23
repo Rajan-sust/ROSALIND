8e174d9c9502b8b0ae2032b060255c568c5b6fef
Rajan-sust <imrajon0@gmail.com>
2019-11-23 14:30:25 +0600

    adding lab7

---
+++ chapter_04/4b.py
@@ -0,0 +1,57 @@
+CODON_TABLE = {
+    'UUU': 'F',     'CUU': 'L',     'AUU': 'I',     'GUU': 'V',
+    'UUC': 'F',     'CUC': 'L',     'AUC': 'I',     'GUC': 'V',
+    'UUA': 'L',     'CUA': 'L',     'AUA': 'I',     'GUA': 'V',
+    'UUG': 'L',     'CUG': 'L',     'AUG': 'M',     'GUG': 'V',
+    'UCU': 'S',     'CCU': 'P',     'ACU': 'T',     'GCU': 'A',
+    'UCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
+    'UCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
+    'UCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
+    'UAU': 'Y',     'CAU': 'H',     'AAU': 'N',     'GAU': 'D',
+    'UAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
+    'UAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
+    'UAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
+    'UGU': 'C',     'CGU': 'R',     'AGU': 'S',     'GGU': 'G',
+    'UGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
+    'UGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
+    'UGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G'
+}
+
+def DNA_to_RNA(genome):
+	return genome.replace('T','U')
+
+def RNA_to_DNA(genome):
+	return genome.replace('U','T')
+
+def get_rev_complement(genome):
+	mp = {
+		'A':'U',
+		'U':'A',
+		'C':'G',
+		'G':'C'
+	}
+	ans = ''
+	for ch in genome:
+		ans+=mp[ch]
+	return ans
+
+def RNA_to_Peptide(genome):
+	return ''.join([CODON_table[genome[idx:idx+3]] for idx in range(0,len(genome),3)])
+
+
+def get_substring(text, pattern):
+	ans = []
+	k = len(pattern)*3
+	rna = dnas
+
+
+
+
+
+def main():
+	text = input()
+	pattern = input()
+
+
+if __name__ == '__main__':
+	main()
